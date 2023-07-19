2d5ac4bc79b989fe3a7549fc0715c3cc53b0d51d
hocineismail <hocineismail75@gmail.com>
2023-07-19 09:56:18 +0100

    implemented themes with styled components

---
+++ ottonova-frontend/src/typed/theme.ts
@@ -0,0 +1,9 @@
+export interface Theme {
+    colors: {
+        primaryBackground: string,
+        secondaryBackground: string,
+        primaryText: string,
+        secondaryText: string
+    };
+
+}
