6536574f8ea469fbe6565ae155cdd6c6310fbcb2
hocineismail <hocineismail75@gmail.com>
2023-07-19 18:11:52 +0100

    Feature: fetch data from API and display it
    Fetch cities data from API and display it on cardsSection. Added loading cards as placeholders until data is available, and implemented error handling to display an error message if the data fetch fails.

---
+++ ottonova-frontend/src/components/sections/CitiesSection.tsx
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+import useFetchDataFromUrl from "../../hooks/useFetchDataFromUrl";
+import Alert from "../alert/Alert";
+import Card from "../card/Card";
+import { City } from "../../typed/cities";
+import CardsSectionPlaceholder from "../loader/CardsPlaceholder";
+import React from "react";
+
+//make sur to add your api on .env
+//NOTICE: if you don't add it, it will use http://localhost:8080/
+//Make sure the backend is running on the same port.
+
+const API = process.env.REACT_APP_API || "http://localhost:8080";
+
+// Styled component representing the grid layout for the cards
+const StyledCardsSection = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 15px;
+  @media (max-width: 1000px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (max-width: 480px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+// Array of elements representing placeholders for the cards
+const placeholders = Array.from({ length: 8 }, (_, index) => (
+  <CardsSectionPlaceholder key={index} />
+));
+
+// Main component for rendering the cities section
+export default function CitiesSection() {
+  // Fetch data from a specified URL using a custom hook
+  const {
+    data: { cities } = { cities: [] },
+    isLoading,
+    errors,
+  } = useFetchDataFromUrl<{ cities: City[] }>({
+    url: `${API}/cities`,
+  });
+
+  // Render placeholders when loading
+  if (isLoading) return <StyledCardsSection>{placeholders}</StyledCardsSection>;
+
+  // Render error message if there are errors
+  // type="danger" to display red alert
+  if (errors) return <Alert text={errors} type="danger" />;
+
+  // Render the grid of city cards if data is available
+  return (
+    <StyledCardsSection>
+      {cities &&
+        cities.map((item: City) => {
+          return <Card key={item.name} {...item} />;
+        })}
+    </StyledCardsSection>
+  );
+}
---
+++ ottonova-frontend/src/hooks/useFetchDataFromUrl.ts
@@ -0,0 +1,50 @@
+
+
+import React from 'react'
+
+
+/**
+ * Custom hook to fetch data from a specified URL.
+ *
+ * @param {Props} props - The hook props.
+ * @param {string} props.url - The URL to fetch the data from.
+ * @returns {Object} - An object containing the fetched data, loading state, and errors.
+ */
+
+interface Props {
+    url: string
+}
+interface ApiResponse<T> {
+    data: T | undefined;
+    isLoading: boolean;
+    errors: string | null;
+}
+export default function useFetchDataFromUrl<T>({ url }: Props): ApiResponse<T> {
+
+    const [data, setData] = React.useState<T | undefined>(undefined);
+    const [isLoading, setIsLoading] = React.useState<boolean>(true);
+    const [errors, setErrors] = React.useState<string | null>(null);
+    React.useEffect(() => {
+        if (url) {
+            (async () => {
+                try {
+                    const response = await fetch(url);
+                    if (response.ok) {
+                        const responseData = await response.json();
+                        setIsLoading(false);
+                        setData(responseData);
+                    } else {
+                        throw new Error('Request failed');
+                    }
+                } catch (error) {
+                    setIsLoading(false);
+                    setErrors('Oooups!!! Something went Wrong');
+                }
+            })();
+        }
+    }, [url]);
+
+    return {
+        data, isLoading, errors
+    }
+}
